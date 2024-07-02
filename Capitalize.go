d6f9b9e13c5d4271177fcfeb91db2e93137fef3b
Sam Caldwell <github@samcaldwell.net>
2024-07-02 11:44:19 -0500

    add go:inline pragma to Captialize()

--- Capitalize.go
+++ Capitalize.go
@@ -2,7 +2,7 @@ package convert
 
 // Capitalize - Capitalize the first character of the string
 //
-//	(c) 2023 Sam Caldwell.  MIT License
+//go:inline
 func Capitalize(s string) string {
 	return Capitalizep(&s)
 }
