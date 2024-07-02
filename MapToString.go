1426a4720684bb134a464e215f8847fc7f0c2d15
Sam Caldwell <github@samcaldwell.net>
2024-07-02 13:41:09 -0500

    improve docstring for MapToString()

--- MapToString.go
+++ MapToString.go
@@ -6,7 +6,11 @@ import (
 	"strings"
 )
 
-// MapToString - Given a map of generic types as a string.
+// MapToString - Given a map of generic types return a string of ampersand (&) delimited key=value pairs
+//
+//	This function will take a map[K]V and return a string, delimited by an ampersand (&)
+//	For example:
+//	   key1=value1&key2=value2&key3=value3
 func MapToString[K string, V any](tags map[K]V) string {
 	var tagPairs []string
 
