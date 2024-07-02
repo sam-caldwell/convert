75f620e25af266c86a2a023bd58c998542f94aa4
Sam Caldwell <github@samcaldwell.net>
2024-07-02 11:23:40 -0500

    add test for ByteSliceToUint64WithLittleEndian()

--- ByteSliceToUint64WithLittleEndian.go
+++ ByteSliceToUint64WithLittleEndian.go
@@ -4,10 +4,8 @@ import "encoding/binary"
 
 // ByteSliceToUint64WithLittleEndian - Convert 8-byte slice to uint64
 //
-//	 This function converts it to uint64.
-//	 This assumes LittleEndian.
-//
-//		(c) 2023 Sam Caldwell.  MIT License
+//	This function converts it to uint64.
+//	This assumes LittleEndian.
 func ByteSliceToUint64WithLittleEndian(b []byte) uint64 {
 	if len(b) != 8 {
 		panic("byte slice must be exactly 8 bytes long")
---
+++ ByteSliceToUint64WithLittleEndian_test.go
@@ -0,0 +1,31 @@
+package convert
+
+import (
+	"testing"
+)
+
+func TestByteSliceToUint64WithLittleEndian(t *testing.T) {
+	// Happy path
+	happyPathInput := []byte{0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	expectedOutput := uint64(1)
+	actualOutput := ByteSliceToUint64WithLittleEndian(happyPathInput)
+	if actualOutput != expectedOutput {
+		t.Errorf("expected %d, got %d", expectedOutput, actualOutput)
+	}
+
+	// Sad path: input length less than 8 bytes
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("expected panic for input length less than 8 bytes, but did not panic")
+		}
+	}()
+	ByteSliceToUint64WithLittleEndian([]byte{0x00, 0x00, 0x00, 0x00})
+
+	// Sad path: input length greater than 8 bytes
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("expected panic for input length greater than 8 bytes, but did not panic")
+		}
+	}()
+	ByteSliceToUint64WithLittleEndian([]byte{0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02})
+}
